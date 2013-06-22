db8b27aaa09e20d70c789f8242ff87e69f94b4fc
latem999 <latem999@gmail.com>
2013-06-22 02:16:08 +0200

    Layout optimisation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    "Ruby on Rails Tutorial Sample App#{ ' | ' + page_title unless page_title.empty?}"
+  end
 end
